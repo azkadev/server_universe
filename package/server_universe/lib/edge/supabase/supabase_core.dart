8e293a70f28d7e6399d76280fd7f53e426636c67
￴￴ <>
2024-05-22 07:36:59 +0700

    up

--- package/server_universe/lib/edge/supabase/supabase_core.dart
+++ package/server_universe/lib/edge/supabase/supabase_core.dart
@@ -40,5 +40,4 @@ export 'package:server_universe/edge/edge.dart';
 import 'package:typings/core.dart' as interop;
 
 @JS('server_universeDartSupabaseFetchHandler')
-external set server_universeDartSupabaseFetchHandler(
-    Promise<interop.Response> Function(interop.Request req) onFunction);
+external set server_universeDartSupabaseFetchHandler(Promise<interop.Response> Function(interop.Request req) onFunction);
