c3e2ca839414518b09d9e3aa691cf1ce070d6139
‎ <>
2024-12-31 08:44:12 +0700

    update

--- package/server_universe/bin/install.dart
+++ package/server_universe/bin/install.dart
@@ -37,6 +37,11 @@ import 'dart:io';
 import 'package:general_lib/general_lib.dart';
 
 void main(List<String> args) {
-  print(Dart.pub.installFromDirectory(
-      directoryPackage: Directory.current, deleteIfExist: true));
+  print(
+    Dart.pub.installFromDirectory(
+      directoryPackage: Directory.current,
+      deleteIfExist: true,
+      archiveSha256: null,
+    ),
+  );
 }
