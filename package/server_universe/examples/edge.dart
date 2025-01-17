bbcaee867115001f0a9f60216568a698751294b8
‎ <>
2025-01-17 19:07:34 +0700

    up

--- package/server_universe/examples/edge.dart
+++ package/server_universe/examples/edge.dart
@@ -1,17 +1,23 @@
-
 import 'package:server_universe/edge/edge.dart';
+
 void main() async {
   print("start");
   ServerUniverseEdge app = ServerUniverseEdge(
     onNotFound: (request, res) async {
-      return res.status(404).json({"@type": "error", "message": "path_not_found", "description": "PATH: ${request.path} Not Found"});
+      return res.status(404).json({
+        "@type": "error",
+        "message": "path_not_found",
+        "description": "PATH: ${request.path} Not Found"
+      });
     },
     onError: (req, res, object, stackTrace) {
-      return res.status(500).json({"@type": "error", "message": "server_crash"});
+      return res
+          .status(500)
+          .json({"@type": "error", "message": "server_crash"});
     },
   );
   app.ensureInitialized();
-  
+
   app.all("/", (req, res) {
     return res.send("oke");
   });
--- package/server_universe/examples/native.dart
+++ package/server_universe/examples/native.dart
@@ -1,7 +1,7 @@
-
 import 'dart:io';
 import 'package:general_lib/extension/dynamic.dart';
-import 'package:server_universe/native.dart'; 
+import 'package:server_universe/native.dart';
+
 void main() async {
   print("start");
   int port = int.tryParse(Platform.environment["PORT"] ?? "3000") ?? 3000;
@@ -10,10 +10,10 @@ void main() async {
     serverUniverseLogType: ServerUniverseLogType.debug,
     onNotFound: (request, res) async {
       return res.status(400).send(({
-        "@type": "error",
-        "message": "path_not_found",
-        "description": "PATH: Not Found",
-      }.toStringifyPretty()));
+            "@type": "error",
+            "message": "path_not_found",
+            "description": "PATH: Not Found",
+          }.toStringifyPretty()));
     },
   );
   app.all("/", (req, res) {
@@ -25,6 +25,6 @@ void main() async {
     count++;
     return await res.status(200).send("\nCOUNT: ${count}");
   });
-  await app.listen(port:port, bindIp: host); 
+  await app.listen(port: port, bindIp: host);
   print("Server on");
 }
